59fb79812865506f76d3e4fc710f67dead653d0c
Warren Galyen <wgalyen@bellhelmets.com>
2020-11-12 15:06:18 -0800

    Changing dir handling for windows

--- lookup.js
+++ lookup.js
@@ -90,7 +90,8 @@ var lookup = {
 		});
 
 		files.forEach(function(filePath){
-			var shortPath = filePath.replace(__dirname +'/content/', '').trim(),
+			var dir = __dirname.replace(/\\/g, "/") + '/content/';
+			var shortPath = filePath.replace(dir, '').trim(),
 				stat = fs.lstatSync(filePath);
 
 			if(stat.isDirectory()){
