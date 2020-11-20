4b0166bcbb7b2c8253c47e794461a96bbe16a6ba
Warren Galyen <wgalyen@bellhelmets.com>
2020-11-20 10:10:43 -0800

    add dynamic sitemap.xml with lastmod

--- app/routes/sitemap.route.js
+++ app/routes/sitemap.route.js
@@ -5,49 +5,56 @@ var url = require('url');
 var path = require('path');
 var fs = require('fs');
 var sm = require('sitemap');
+var _ = require('underscore');
+var moment = require('moment');
 
-function route_sitemap(config, lookup) {
+function route_sitemap (config, lookup) {
   return function (req, res, next) {
-      var hostname = req.headers.host;
+    var hostname = req.headers.host;
+    var content_dir = path.normalize(lookup.config.content_dir);
 
-      var content_dir = path.normalize(lookup.config.content_dir);
+    // get list md files
+    var files = listFiles(content_dir);
+    files = _.filter(files, function (file) {
+      return file.endsWith('.md');
+    });
 
-      // get list md files
-      var files = listFiles(content_dir, '.md');
-      files = files.map(function (file) {
-          return file.replace(content_dir, '');
+    var filesPath = files.map(function (file) {
+      return file.replace(content_dir, '');
+    });
+
+    // generate list urls
+    var urls = filesPath.map(function (file) {
+      return '/' + file.replace('.md', '').replace('\\', '/');
+    });
+
+    // create sitemap.xml
+    var sitemap = sm.createSitemap({
+      hostname: 'http://' + hostname,
+      cacheTime: 600000
+    });
+
+    for (var i = 0, len = urls.length; i < len; i++) {
+      var stat = fs.lstatSync(files[i]);
+      sitemap.add({
+        url: urls[i],
+        changefreq: 'weekly',
+        priority: 0.8,
+        lastmod: moment(stat.mtime).format('YYYY-MM-DD')
       });
+    }
 
-      // generate list urls
-      var urls = files.map(function (file) {
-          return '/' + file.replace('.md', '').replace('\\', '/');
-      });
-
-      // create sitemap.xml
-      var sitemap = sm.createSitemap({
-          hostname: 'http://' + hostname,
-          cacheTime: 600000
-      });
-
-      for (var i = 0, len = urls.length; i < len; i++) {
-          sitemap.add({
-              url: urls[i],
-              changefreq: 'weekly',
-              priority: 0.8
-          });
-      }
-
-      res.header('Content-Type', 'application/xml');
-      res.send(sitemap.toString());
+    res.header('Content-Type', 'application/xml');
+    res.send(sitemap.toString());
   };
 }
 
-function listFiles(dir, suffix) {
-    return fs.readdirSync(dir).reduce(function (list, file) {
-        var name = path.join(dir, file);
-        var isDir = fs.statSync(name).isDirectory();
-        return list.concat(isDir ? listFiles(name) : [name]);
-    }, []);
+function listFiles (dir) {
+  return fs.readdirSync(dir).reduce(function (list, file) {
+    var name = path.join(dir, file);
+    var isDir = fs.statSync(name).isDirectory();
+    return list.concat(isDir ? listFiles(name) : [name]);
+  }, []);
 }
 
 // Exports
