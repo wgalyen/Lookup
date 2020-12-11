fd7a7cb5d90e83512f6656f77dc43ac21a435137
Warren Galyen <wgalyen@bellhelmets.com>
2020-12-11 12:47:46 -0800

    Visibility of menu on pages controlled by config setting

--- example/config.default.js
+++ example/config.default.js
@@ -115,8 +115,10 @@ var config = {
       // append: 'Table of contents appendix',
       // maxdepth: 6,
       // firsth1: true,
-    }
+    },
 
+    menu_on_pages: true,
+    menu_on_page_collapsible: true
 };
 config.public_dir = __dirname + '/../themes/' + config.theme_name + '/public/';
 
